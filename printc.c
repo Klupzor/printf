15f48d93e662d4f9d1589b3e7f1835cc14cce701
klupzor <lopezgand@gmail.com>
2019-03-19 16:47:32 +0000

    Sending format counter to function to print

--- printc.c
+++ printc.c
@@ -1,30 +1,26 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * printc - print character
  *
  * @wsize: poninter to add writen_size.
  * @c: character to print.
  * @c2: next character to evaluate conditions.
+ * @fc: pointer to format counter.
  *
- * Return: 0 when print one char,
- * 1 when print spacial case,
+ * Return: 1 when print case,
  * and -1 when it fail.
  */
-int printc(unsigned int *wsize, char c, char c2)
+int printc(unsigned int *wsize, unsigned int *fc, char c, char c2)
 {
 	char str[2];
 
 	if (c == '%' && c2 == '%')
-	{
-		c  = '%';
-		return (1);
-	}
+		*fc += 1;
 	if (c == '%' && c2 == '\0')
 		return (-1);
-
 	str[0] = c;
 	str[1] = '\0';
 	put_output(wsize, str);
-	return (0);
+	return (1);
 }
