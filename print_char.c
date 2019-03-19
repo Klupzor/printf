d32c9b0fc6c8db4d7f06cba4984a0a7dea1b3022
arleybri18 <arleybri18@gmail.com>
2019-03-19 00:20:32 +0000

    [features] Functions int, digit, char
    function that handle print, convert numbers, add to holberton.h

---
+++ print_char.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_char - return a string with string received
+ *
+ * @type_struct: string that contain flags and type of structure
+ * @output: string contain the characteres to print
+ * @args: argument to print
+ *
+ * Return: return 1 if successfull
+ */
+
+int print_char(char *type_struct, unsigned int *output, va_list args)
+{
+    	(void)*type_struct;
+	char str[2];
+	char c = va_arg(args, int);
+	str[0] = c;
+	str[1] = '\0';
+
+	put_output(output, str);
+
+	return (1);
+
+
+}
